ed8013a663e9e5d8d643865046cb9f0dd8e3c98b
aziubin <aziubin@googlemail.com>
2023-05-17 20:52:42 +0300

    Multiple kafka templates. Same topic consumer. Serialize exception.

--- kafka/src/main/java/com/aziubin/spring/boot/kafka/KafkaApplication.java
+++ kafka/src/main/java/com/aziubin/spring/boot/kafka/KafkaApplication.java
@@ -1,13 +1,21 @@
 package com.aziubin.spring.boot.kafka;
 
+import java.io.IOException;
+import java.time.LocalDateTime;
 import java.util.UUID;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import domain.CustomerVisitEvent;
+
+import org.springframework.kafka.annotation.KafkaListener;
 import org.springframework.kafka.core.KafkaTemplate;
 
 @SpringBootApplication
@@ -19,17 +27,31 @@ public class KafkaApplication {
 
 	@RestController
 	static class RestfulController {
+	    private static final String CUSTOMER_VISIT_EVENT = "customer_visit_event";
+
+        Logger logger = LoggerFactory.getLogger(RestfulController.class);
+
 	    @Autowired
-	    KafkaTemplate<String, String> kafkaTemplate;
+	    KafkaTemplate<String, CustomerVisitEvent> customerVisitEventKafkaTemplate;
+
+	    @Autowired
+	    KafkaTemplate<String, String> stringkafkaTemplate;
+
+	    @KafkaListener(topics = CUSTOMER_VISIT_EVENT, groupId = "group_id")
+	    public void consume(String message) throws IOException {
+	        logger.info(String.format("#### -&gt; Consumed message -&gt; %s", message));
+	    }
 
 	    @GetMapping("/")
 	    void get() {
-	        kafkaTemplate.send("customer_visit_event", UUID.randomUUID().toString());
+
+	        stringkafkaTemplate.send(CUSTOMER_VISIT_EVENT, UUID.randomUUID().toString());
 	    }
 
 	    @GetMapping("/customer_visit_event")
 	    void getCustomerVisitEvent() {
-	        kafkaTemplate.send("customer_visit_event", UUID.randomUUID().toString());
+	        CustomerVisitEvent customerVisitEvent = CustomerVisitEvent.builder().customerId(UUID.randomUUID().toString()).dateTime(LocalDateTime.now()).build();
+	        customerVisitEventKafkaTemplate.send(CUSTOMER_VISIT_EVENT, customerVisitEvent);
 	    }
 	}
 	
