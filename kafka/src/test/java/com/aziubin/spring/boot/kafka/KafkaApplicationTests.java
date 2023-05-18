325638608216d2fd9a832c6b9fd258b013ee67aa
aziubin <aziubin@googlemail.com>
2023-05-18 17:49:40 +0300

    unit test produce uuid message, wait consumer and compare payload.

--- kafka/src/test/java/com/aziubin/spring/boot/kafka/KafkaApplicationTests.java
+++ kafka/src/test/java/com/aziubin/spring/boot/kafka/KafkaApplicationTests.java
@@ -1,13 +1,42 @@
 package com.aziubin.spring.boot.kafka;
 
+import java.util.concurrent.ArrayBlockingQueue;
+import java.util.concurrent.BlockingQueue;
+import java.util.concurrent.ExecutionException;
+import java.util.concurrent.TimeUnit;
+import java.util.concurrent.TimeoutException;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertNotNull;
+
+import org.apache.kafka.common.Uuid;
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.kafka.annotation.KafkaListener;
+import org.springframework.kafka.core.KafkaTemplate;
 
 @SpringBootTest
 class KafkaApplicationTests {
+    private static final String CUSTOMER_VISIT_EVENT = "customer_visit_event";
+//    private final List<String> messageList = new ArrayList<>();
+    private final BlockingQueue<String> q = new ArrayBlockingQueue<>(15);
+
+    @Autowired
+    KafkaTemplate<String, String> stringkafkaTemplate;
+
+    @KafkaListener(topics = CUSTOMER_VISIT_EVENT)
+    void consume(String message) throws InterruptedException {
+        q.put(message);
+    }
 
 	@Test
-	void contextLoads() {
+	void contextLoads() throws InterruptedException, ExecutionException, TimeoutException {
+	    String payload = Uuid.randomUuid().toString();
+	    stringkafkaTemplate.send(CUSTOMER_VISIT_EVENT, payload).get(10, TimeUnit.SECONDS);
+	    String s = q.poll(10, TimeUnit.SECONDS);
+	    assertNotNull(s);
+	    assertEquals(payload, s);
 	}
 
 }
